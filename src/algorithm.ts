ac3856eeab7ba0e7f6c6da45b03e1c259f7880bb
Federico Baldini <federico.baldini7@gmail.com>
2023-05-01 11:23:20 +0200

    Update algorithm.ts
    Added multiple variable, minMax function to normalize the features and the nDimensionalDistance for calculate the distance between n-points.

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,14 +1,94 @@
 /**
  * K-Nearest Neighbor algorithm implementation in TypeScript.
- * (using a single indipendent variable)
  */
 
-import { chain, shuffle } from "lodash";
+import { chain, shuffle, last, initial, cloneDeep, min, max } from "lodash";
 
 type Dataset = Array<Array<number>>;
 
-const distance = (pointA: number, pointB: number): number => {
-  return Math.abs(pointA - pointB);
+/**
+ * This function scales the values of a dataset to a value between 0 and 1 based on
+ * the minimum and maximum values for each feature column.
+ *
+ * @param dataset The dataset to be scaled.
+ * @param featureCount The number of features in the dataset.
+ * @returns The scaled dataset.
+ */
+const minMax = (dataset: Dataset, featureCount: number): Dataset => {
+  // Create a deep clone of the dataset to avoid modifying the original dataset.
+  const clonedDataset: Dataset = cloneDeep(dataset);
+
+  // Iterate over each feature column in the dataset.
+  for (
+    let columnIndex: number = 0;
+    columnIndex < featureCount;
+    columnIndex += 1
+  ) {
+    // Extract the column data for the current feature.
+    const columnData: Array<number> = clonedDataset.map(
+      (rowData: Array<number>) => rowData[columnIndex]
+    );
+    // Calculate the minimum and maximum values for the feature.
+    const minValue: number | undefined = min(columnData);
+    const maxValue: number | undefined = max(columnData);
+
+    // If both minimum and maximum values are defined, scale the feature values for each data point in the column.
+    if (maxValue !== undefined && minValue !== undefined) {
+      for (
+        let rowIndex: number = 0;
+        rowIndex < clonedDataset.length;
+        rowIndex += 1
+      ) {
+        clonedDataset[rowIndex][columnIndex] =
+          (clonedDataset[rowIndex][columnIndex] - minValue) /
+          (maxValue - minValue);
+      }
+    }
+  }
+
+  return clonedDataset;
+};
+
+/**
+ * This function calculates the n-dimensional distance between two points using
+ * the Pythagorean theorem for n-dimensional triangles.
+ *
+ * @param pointsA An array representing the coordinates of the first point.
+ * @param pointsB An array representing the coordinates of the second point.
+ * @returns The distance between the two points as a number.
+ * @throws An error if pointsA and pointsB do not have the same size.
+ */
+const nDimensionalDistance = (
+  pointsA: Array<number>,
+  pointsB: Array<number>
+): number | undefined => {
+  if (pointsA.length === pointsB.length) {
+    /*
+      - ZIP:
+        Creates an array of pairs of corresponding elements from the two input arrays (pointsA and pointsB).
+        Example: [ [ 1, 2, 3 ], [ 4, 5, 6 ] ] becomes [ [ 1, 4 ], [ 2, 5 ], [ 3, 6 ] ]
+
+      - MAP:
+        Calculates the square of the difference between the elements of each pair in the array of pairs
+        created by the "zip" function.
+        Example: [ [ 1, 4 ], [ 2, 5 ], [ 3, 6 ] ] becomes [ 9, 9, 9 ]
+
+      - SUM:
+        Sum the squared differences of the pairs obtained from the "map" function,
+        and then calculate the square root of the result which represents the n-dimensional distance between the two points.
+        Example: 27
+    */
+    return (
+      chain(pointsA)
+        .zip(pointsB)
+        .map(([a, b]: [number, number]) => (a - b) ** 2)
+        .sum()
+        .value() ** 0.5
+    );
+  }
+  throw new Error(
+    "nDimensionalDistance: the two points array must have the same size."
+  );
 };
 
 const splitDataset = (
@@ -45,12 +125,16 @@ const splitDataset = (
  * @param k k-nearest neighbors.
  * @returns The predicted label for the test point.
  */
-const knn = (dataset: Dataset, predictionPoint: number, k: number): number => {
+const knn = (
+  dataset: Dataset,
+  predictionPoints: Array<number>,
+  k: number
+): number => {
   /*
     - MAP:
-      Calculates the distance between each training data point and the prediction point,
-      and maps each training data point to a new array that pairs its distance with its "bucket" value.
-      Returns an array of pairs [distance, bucket].
+      Calculates the distance between each training data point and the prediction point(s),
+      and maps each training data point to a new array that pairs its distance with its dependent variable value.
+      Returns an array of pairs [distance, dependent variable].
       Example: [ [ 5, 2 ], [ 15, 3 ], [ 20, 2 ] ]
 
     - SORT_BY:
@@ -62,36 +146,39 @@ const knn = (dataset: Dataset, predictionPoint: number, k: number): number => {
       Example: [ [ 5, 2 ], [ 15, 3 ], [ 20, 2 ] ] (with k = 2) becomes [ [ 5, 2 ], [ 15, 3 ] ]
 
     - COUNT_BY:
-      Groups the k-nearest neighbors by their "bucket" value, and counts the occurrences of each bucket value.
-      Returns an object whose keys are the unique bucket values, and whose values are the frequencies
-      of those bucket values among the k-nearest neighbors.
+      Groups the k-nearest neighbors by their dependent variable value, and counts the occurrences of each value.
+      Returns an object whose keys are the unique dependent variable values, and whose values are the frequencies
+      of those values among the k-nearest neighbors.
       Example: { "2": 2, "3": 1 }
 
     - TO_PAIRS:
-      Converts the object returned by countBy to an array of pairs [bucket, frequency], in ascending order of frequency.
-      Example: [ [ "3", 1 ], [ "2", 2 ] ]
+      Converts the object returned by countBy to an array of pairs [dependent variable, frequency].
+      Example: [ [ "3", "1" ], [ "2", "2" ] ]
 
     - SORT_BY:
       Sorts the array of pairs by frequency in descending order.
-      Example: [ [ "2", 2 ], [ "3", 1 ] ]
+      Example: [ [ "2", "2" ], [ "3", "1" ] ]
 
     - LAST:
       Takes the last (i.e. highest frequency) pair from the sorted array.
-      Example: [ "2", 2 ] (when there are ties, this function will pick one at random)
+      Example: [ "3", "1" ] (when there are ties, this function will pick one at random)
 
     - FIRST:
-      Takes the first element (i.e. the bucket value) from the last pair.
-      Example: "2"
+      Takes the first element (i.e. the dependent variable value) from the last pair.
+      Example: "3"
 
     - PARSE_INT:
-      Converts the string value of the bucket to a number.
-      Example: 2
+      Converts the string value of the dependent variable to a number.
+      Example: 3
   */
   return chain(dataset)
-    .map((data: Array<number>) => [distance(data[0], predictionPoint), data[3]])
-    .sortBy((data: Array<number>) => data[0])
+    .map((data: Array<number>) => [
+      nDimensionalDistance(initial(data), predictionPoints),
+      last(data),
+    ])
+    .sortBy((data: Array<number | undefined>) => data[0])
     .slice(0, k)
-    .countBy((data: Array<number>) => data[1])
+    .countBy((data: Array<number | undefined>) => data[1])
     .toPairs()
     .sortBy((data: Array<string | number>) => data[1])
     .last()
@@ -123,11 +210,11 @@ const knn = (dataset: Dataset, predictionPoint: number, k: number): number => {
 const analyzeDataset = (dataset: Dataset): void => {
   const testDatasetSize: number = 100;
   const [testDataset, trainingDataset]: [Dataset, Dataset] = splitDataset(
-    dataset,
+    minMax(dataset, 3),
     testDatasetSize
   );
 
-  for (let k = 1; k < 20; k += 1) {
+  for (let k: number = 1; k < 20; k += 1) {
     /*
       - FILTER:
         Filters the test dataset by selecting only the test data points for which
@@ -144,7 +231,7 @@ const analyzeDataset = (dataset: Dataset): void => {
     const accuracy: number = chain(testDataset)
       .filter(
         (testData: Array<number>) =>
-          knn(trainingDataset, testData[0], k) === testData[3]
+          knn(trainingDataset, initial(testData), k) === last(testData)
       )
       .size()
       .divide(testDatasetSize)
