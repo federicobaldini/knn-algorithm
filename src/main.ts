a31c401fef547077b52d11218372b4287519d4aa
Federico Baldini <federico.baldini7@gmail.com>
2023-05-01 11:23:56 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { analyzeDataset } from "./algorithm";
 
-let dataToAnalyze: Array<Array<number>> = [];
+type Dataset = Array<Array<number>>;
+
+let datasetToAnalyze: Dataset = [];
 
 const fetchDataset = async (): Promise<void> => {
   await fetch("/dataset.json")
     .then((response) => response.json())
     .then((data) => {
-      dataToAnalyze = data.dataset;
+      datasetToAnalyze = data.dataset;
     })
     .catch((error) => {
       console.error(error);
@@ -15,8 +17,7 @@ const fetchDataset = async (): Promise<void> => {
 
 const start = async (): Promise<void> => {
   await fetchDataset();
-  const predictedLabel: number = analyzeDataset(dataToAnalyze);
-  console.log(predictedLabel);
+  analyzeDataset(datasetToAnalyze);
 };
 
 start();
