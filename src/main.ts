6dc5a8eaa0f7b59946847b0c58a1c06c7e209bd4
Federico Baldini <federico.baldini7@gmail.com>
2023-05-07 00:35:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,55 @@
-import { analyzeDataset } from "./algorithm";
+import { tensor } from "@tensorflow/tfjs";
+import { loadCSV } from "./csv-loader";
+import { knn } from "./algorithm";
 
-type Dataset = Array<Array<number>>;
-
-let datasetToAnalyze: Dataset = [];
-
-const fetchDataset = async (): Promise<void> => {
-  await fetch("/dataset.json")
-    .then((response) => response.json())
-    .then((data) => {
-      datasetToAnalyze = data.dataset;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+type Dataset = Array<Array<string | number>>;
 
 const start = async (): Promise<void> => {
-  await fetchDataset();
-  analyzeDataset(datasetToAnalyze);
+  let {
+    features,
+    labels,
+    testFeatures,
+    testLabels,
+  }: {
+    features: Dataset;
+    labels: Dataset;
+    testFeatures?: Dataset;
+    testLabels?: Dataset;
+  } = loadCSV("kc_house_data.csv", {
+    datasetColumns: ["lat", "long", "sqft_lot", "sqft_living"],
+    labelColumns: ["price"],
+    shuffle: true,
+    splitTest: 10,
+  });
+
+  if (testFeatures !== undefined) {
+    testFeatures.forEach(
+      (testFeature: Array<string | number>, index: number): void => {
+        const result: number = knn(
+          tensor(features),
+          tensor(labels),
+          tensor(testFeature),
+          10
+        );
+        if (testLabels !== undefined) {
+          const error: number =
+            ((testLabels[index][0] as number) - result) /
+            (testLabels[index][0] as number);
+          if (Math.abs(error * 100) < 10) {
+            console.log(
+              `Success - Error for feature ${index}: ${(error * 100).toFixed(
+                2
+              )}%`
+            );
+          } else {
+            console.log(
+              `Error for feature ${index}: ${(error * 100).toFixed(2)}%`
+            );
+          }
+        }
+      }
+    );
+  }
 };
 
 start();
