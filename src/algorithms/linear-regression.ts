5cc2f95d8c2292f1c4b017a1688460594a890063
Federico Baldini <federico.baldini7@gmail.com>
2023-05-14 11:49:38 +0200

    Update linear-regression.ts

--- src/algorithms/linear-regression.ts
+++ src/algorithms/linear-regression.ts
@@ -2,6 +2,7 @@ import { Tensor, Rank } from "@tensorflow/tfjs";
 
 type Options = {
   learningRate: number;
+  iterations: number;
 };
 
 class LinearRegression {
@@ -9,10 +10,21 @@ class LinearRegression {
   private labels: Tensor<Rank>;
   private options: Options;
 
-  constructor(features: Tensor<Rank>, labels: Tensor<Rank>, options: Options) {
+  constructor(features: Tensor<Rank>, labels: Tensor<Rank>, options?: Options) {
     this.features = features;
     this.labels = labels;
-    this.options = options;
+    this.options = Object.assign(
+      { learningRate: 0.1, iterations: 1000 },
+      options
+    );
+  }
+
+  gradientDescent(): void {}
+
+  train(): void {
+    for (let i = 0; i < this.options.iterations; i += 1) {
+      this.gradientDescent();
+    }
   }
 }
 
