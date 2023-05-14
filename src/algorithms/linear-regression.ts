0ec6a0dbf4630668144e47df76e3b585ddecec70
Federico Baldini <federico.baldini7@gmail.com>
2023-05-14 11:49:59 +0200

    Update linear-regression.ts

--- src/algorithms/linear-regression.ts
+++ src/algorithms/linear-regression.ts
@@ -5,21 +5,37 @@ type Options = {
   iterations: number;
 };
 
-class LinearRegression {
-  private features: Tensor<Rank>;
-  private labels: Tensor<Rank>;
-  private options: Options;
+type Dataset = Array<Array<string | number>>;
 
-  constructor(features: Tensor<Rank>, labels: Tensor<Rank>, options?: Options) {
+class LinearRegression {
+  private features: Dataset;
+  private labels: Dataset;
+  private options: Options;
+  private m: number;
+  private b: number;
+
+  constructor(features: Dataset, labels: Dataset, options?: Options) {
     this.features = features;
     this.labels = labels;
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
       options
     );
+    this.m = 0;
+    this.b = 0;
   }
 
-  gradientDescent(): void {}
+  gradientDescent(): void {
+    // TODO: generalize it
+    const currentGuessesForMPG = this.features.map(
+      (row: Array<string | number>): number => {
+        if (Number(row[0])) {
+          return this.m * (row[0] as number) + this.b;
+        }
+        return 0;
+      }
+    );
+  }
 
   train(): void {
     for (let i = 0; i < this.options.iterations; i += 1) {
