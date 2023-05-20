94c1b5e83242c415a94ded681c7b4012c926b18d
Federico Baldini <federico.baldini7@gmail.com>
2023-05-20 12:24:17 +0200

    Update linear-regression.ts
    Calculated the slope of the Mean Squared Error (MSE) with respect to y-intercept (b).

--- src/algorithms/linear-regression.ts
+++ src/algorithms/linear-regression.ts
@@ -35,6 +35,21 @@ class LinearRegression {
         return 0;
       }
     );
+
+    // Calculate the slope of the Mean Squared Error (MSE) with respect to y-intercept (b).
+    const mseSlopeByB: number =
+      (sum(
+        currentGuessesForMPG.map((guess: number, index: number): number => {
+          if (Number(this.labels[index][0])) {
+            // Calculate the partial derivative of the error with respect to y-intercept (b).
+            return guess - (this.labels[index][0] as number);
+          }
+          // Return 0 for non-numeric values.
+          return 0;
+        })
+      ) *
+        2) /
+      this.features.length;
   }
 
   train(): void {
