fe3d2939c461d2fcf74bd01bbb88984c3f533026
Federico Baldini <federico.baldini7@gmail.com>
2023-05-27 15:02:25 +0200

    Update linear-regression.ts
    Used TensorFlow to refactor the class.

--- src/algorithms/linear-regression.ts
+++ src/algorithms/linear-regression.ts
@@ -1,13 +1,10 @@
-import { sum } from "lodash";
+import { Tensor, Rank, ones, zeros } from "@tensorflow/tfjs";
 
 // Define the Options type for configuration.
 type Options = {
   learningRate: number;
   iterations: number;
 };
-
-// Define the Dataset type for input data.
-type Dataset = Array<Array<string | number>>;
 
 /**
  * Represents a linear regression model.
@@ -16,11 +13,10 @@ type Dataset = Array<Array<string | number>>;
  * and dependent variables (labels) using gradient descent optimization.
  */
 class LinearRegression {
-  private features: Dataset;
-  private labels: Dataset;
+  private features: Tensor<Rank>;
+  private labels: Tensor<Rank>;
   private options: Options;
-  private m: number;
-  private b: number;
+  private weights: Tensor<Rank>; // Contains the slope (m) and y-intercept (b) values.
 
   /**
    * Creates an instance of LinearRegression.
@@ -29,82 +25,42 @@ class LinearRegression {
    * @param labels - The training data labels.
    * @param options - The configuration options for the model.
    */
-  constructor(features: Dataset, labels: Dataset, options?: Options) {
-    this.features = features;
+  constructor(features: Tensor<Rank>, labels: Tensor<Rank>, options?: Options) {
+    this.features = features.concat(ones([features.shape[0], 1]), 1);
     this.labels = labels;
+
     // Set default options if not provided.
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
       options
     );
-    // Set initial slope (m) and y-intercept (b) values.
-    this.m = 0;
-    this.b = 0;
+
+    this.weights = zeros([2, 1]);
   }
 
   getM(): number {
-    return this.m;
+    return (this.weights.arraySync() as Array<number>)[0];
   }
 
   getB(): number {
-    return this.b;
+    return (this.weights.arraySync() as Array<number>)[1];
   }
 
   /**
    * Performs gradient descent optimization to update the model parameters.
    */
   gradientDescent(): void {
-    // TODO: generalize it
+    const currentGuesses: Tensor<Rank> = this.features.matMul(this.weights);
+    const differences: Tensor<Rank> = currentGuesses.sub(this.labels);
 
-    // Calculate the current guesses for the dependent variable.
-    const currentGuessesForMPG = this.features.map(
-      (row: Array<string | number>): number => {
-        if (Number(row[0])) {
-          // Calculate the predicted value (guess) for a row.
-          return this.m * (row[0] as number) + this.b;
-        }
-        // Return 0 for non-numeric values.
-        return 0;
-      }
-    );
+    // Calculate the slopes (gradients) using the differences and features.
+    const slopes: Tensor<Rank> = this.features
+      .transpose()
+      .matMul(differences)
+      .div(this.features.shape[0]);
 
-    // Calculate the slope of the Mean Squared Error (MSE) with respect to y-intercept (b).
-    const mseSlopeByB: number =
-      (sum(
-        currentGuessesForMPG.map((guess: number, index: number): number => {
-          if (Number(this.labels[index][0])) {
-            // Calculate the partial derivative of the error with respect to y-intercept (b).
-            return guess - (this.labels[index][0] as number);
-          }
-          // Return 0 for non-numeric values.
-          return 0;
-        })
-      ) *
-        2) /
-      this.features.length;
-
-    // Calculate the slope of the Mean Squared Error (MSE) with respect to slope (m).
-    const mseSlopeByM: number =
-      (sum(
-        currentGuessesForMPG.map((guess: number, index: number): number => {
-          if (Number(this.features[index][0])) {
-            // Calculate the partial derivative of the error with respect to slope (m).
-            return (
-              -1 *
-              (this.features[index][0] as number) *
-              ((this.labels[index][0] as number) - guess)
-            );
-          }
-          // Return 0 for non-numeric values.
-          return 0;
-        })
-      ) *
-        2) /
-      this.features.length;
-
-    // Update the slope (m) and y-intercept (b) values using gradient descent.
-    this.m = this.m - mseSlopeByM * this.options.learningRate;
-    this.b = this.b - mseSlopeByB * this.options.learningRate;
+    // Update the weights using the calculated slopes and learning rate.
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   /**
