12b76028905da9a231d8ce1a7913ad7a949d99e3
Federico Baldini <federico.baldini7@gmail.com>
2023-06-18 12:06:02 +0200

    Update binary-logistic-regression.ts
    Added more tensor rank detail.

--- src/algorithms/binary-logistic-regression.ts
+++ src/algorithms/binary-logistic-regression.ts
@@ -15,12 +15,12 @@ type Options = {
  * and dependent variables (labels) using gradient descent optimization.
  */
 class BinaryLogisticRegression {
-  private features: Tensor<Rank>; // Input features for training data.
-  private labels: Tensor<Rank>; // Output labels for training data.
+  private features: Tensor<Rank.R2>; // Input features for training data.
+  private labels: Tensor<Rank.R2>; // Output labels for training data.
   private options: Options; // Configuration options for the model.
-  private weights: Tensor<Rank>; // Contains the slope (m) and y-intercept (b) values.
-  private mean: Tensor<Rank> | undefined; // Mean tensor for standardization.
-  private variance: Tensor<Rank> | undefined; // Variance tensor for standardization.
+  private weights: Tensor<Rank.R2>; // Contains the slope (m) and y-intercept (b) values.
+  private mean: Tensor<Rank.R1> | undefined; // Mean tensor for standardization.
+  private variance: Tensor<Rank.R1> | undefined; // Variance tensor for standardization.
   private costHistory: Array<number>; // Stores the history of cross entropy during training.
 
   /**
@@ -30,7 +30,11 @@ class BinaryLogisticRegression {
    * @param labels - The training data labels.
    * @param options - The configuration options for the model.
    */
-  constructor(features: Tensor<Rank>, labels: Tensor<Rank>, options?: Options) {
+  constructor(
+    features: Tensor<Rank.R2>,
+    labels: Tensor<Rank.R2>,
+    options?: Options
+  ) {
     this.features = this.initFeatures(features);
     this.labels = labels;
     this.costHistory = [];
@@ -65,17 +69,17 @@ class BinaryLogisticRegression {
   /**
    * Performs gradient descent optimization to update the model parameters.
    */
-  gradientDescent(features: Tensor<Rank>, labels: Tensor<Rank>): void {
+  gradientDescent(features: Tensor<Rank.R2>, labels: Tensor<Rank.R2>): void {
     // Calculate the current predictions
-    const currentGuesses: Tensor<Rank> = features
+    const currentGuesses: Tensor<Rank.R2> = features
       .matMul(this.weights)
-      .sigmoid();
+      .sigmoid() as Tensor<Rank.R2>;
 
     // Calculate the differences between predictions and labels
-    const differences: Tensor<Rank> = currentGuesses.sub(labels);
+    const differences: Tensor<Rank.R2> = currentGuesses.sub(labels);
 
     // Calculate the slope (gradients) of MSE using the differences and features
-    const slope: Tensor<Rank> = features
+    const slope: Tensor<Rank.R2> = features
       .transpose()
       .matMul(differences)
       .div(features.shape[0]);
@@ -102,7 +106,7 @@ class BinaryLogisticRegression {
         // Calculate the starting index of the current batch
         const startIndex: number = j * batchSize;
         // Create a slice of features tensor corresponding to the current batch
-        const featuresSlice: Tensor<Rank> = this.features.slice(
+        const featuresSlice: Tensor<Rank.R2> = this.features.slice(
           [startIndex, 0],
           [batchSize, -1]
         );
@@ -127,13 +131,13 @@ class BinaryLogisticRegression {
    * @param observations - The input observations for prediction.
    * @returns The predicted output labels.
    */
-  predict(observations: Tensor<Rank>): Tensor<Rank> {
+  predict(observations: Tensor<Rank.R2>): Tensor<Rank.R2> {
     // Initialize the features tensor for the input observations
-    const features: Tensor<Rank> = this.initFeatures(observations);
+    const features: Tensor<Rank.R2> = this.initFeatures(observations);
 
     // Perform matrix multiplication of the features tensor and weights tensor
     // to generate the predicted output labels
-    const predictions: Tensor<Rank> = features
+    const predictions: Tensor<Rank.R2> = features
       .matMul(this.weights)
       .sigmoid()
       .greater(this.options.decisionBoundary as number)
@@ -148,8 +152,8 @@ class BinaryLogisticRegression {
    * @param testFeatures - The test data features.
    * @param testLabels - The test data labels.
    */
-  test(testFeatures: Tensor<Rank>, testLabels: Tensor<Rank>): number {
-    const predictions: Tensor<Rank> = this.predict(testFeatures);
+  test(testFeatures: Tensor<Rank.R2>, testLabels: Tensor<Rank.R2>): number {
+    const predictions: Tensor<Rank.R2> = this.predict(testFeatures);
     const incorrect: number = predictions
       .sub(testLabels)
       .abs()
@@ -165,7 +169,7 @@ class BinaryLogisticRegression {
    * @param features - The input features tensor.
    * @returns The modified features tensor with an additional column of ones.
    */
-  initFeatures(features: Tensor<Rank>): Tensor<Rank> {
+  initFeatures(features: Tensor<Rank.R2>): Tensor<Rank.R2> {
     // Standardize the features tensor
     features = this.standardize(features);
 
@@ -180,14 +184,14 @@ class BinaryLogisticRegression {
    *
    * @param features - The input features tensor.
    */
-  initStandardizationParameters(features: Tensor<Rank>): void {
+  initStandardizationParameters(features: Tensor<Rank.R2>): void {
     // Calculate the mean and variance tensors
     const { mean, variance }: { mean: Tensor<Rank>; variance: Tensor<Rank> } =
       moments(features, 0);
 
     // Store the mean and variance tensors
-    this.mean = mean;
-    this.variance = variance;
+    this.mean = mean as Tensor<Rank.R1>;
+    this.variance = variance as Tensor<Rank.R1>;
   }
 
   /**
@@ -196,7 +200,7 @@ class BinaryLogisticRegression {
    * @param features - The input features tensor.
    * @returns The standardized features tensor.
    */
-  standardize(features: Tensor<Rank>): Tensor<Rank> {
+  standardize(features: Tensor<Rank.R2>): Tensor<Rank.R2> {
     // Check if the mean and variance tensors are not initialized
     if (!this.mean || !this.variance) {
       // Calculate and store the mean and variance tensors
@@ -221,7 +225,9 @@ class BinaryLogisticRegression {
    */
   recordCost(): void {
     // Calculate the current predictions (probabilities) using sigmoid activation
-    const guesses: Tensor<Rank> = this.features.matMul(this.weights).sigmoid();
+    const guesses: Tensor<Rank.R2> = this.features
+      .matMul(this.weights)
+      .sigmoid() as Tensor<Rank.R2>;
 
     // Calculate the termOne of the cross entropy cost
     const termOne = this.labels.transpose().matMul(guesses.log());
