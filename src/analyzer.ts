a856aff9c2e7240711398e2539c42c0277f86562
Federico Baldini <federico.baldini7@gmail.com>
2023-06-21 20:16:15 +0200

    Update analyzer.ts

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -5,11 +5,18 @@ import { LinearRegression } from "./algorithms/linear-regression";
 import { BinaryLogisticRegression } from "./algorithms/binary-logistic-regression";
 import { MultinominalLogisticRegression } from "./algorithms/multinominal-logistic-regression";
 import { flatMap } from "lodash";
-import { training } from "mnist-data";
+import mnist from "mnist-data";
 
 type Dataset = Array<Array<string | number>>;
 
+/**
+ * Run k-nearest neighbors analysis.
+ *
+ * Predicts output labels for test data using the k-nearest neighbor algorithm.
+ * Calculates the percentage error between predicted and actual labels (if available).
+ */
 const runKnnAnalysis = async (): Promise<void> => {
+  // Load CSV file "house.csv" containing training and test data
   let {
     features, // input features for training data.
     labels, // output labels for training data.
@@ -31,6 +38,12 @@ const runKnnAnalysis = async (): Promise<void> => {
   if (testFeatures !== undefined) {
     // Iterate through each row of test features.
     testFeatures.forEach(
+      /**
+       * Predict the output label for each test feature using k-nearest neighbors algorithm.
+       *
+       * @param testFeature - The test input feature.
+       * @param index - The index of the test feature.
+       */
       (testFeature: Array<string | number>, index: number): void => {
         // Use the k-nearest neighbor algorithm to predict output label for test data.
         const result: number = knn(
@@ -39,12 +52,14 @@ const runKnnAnalysis = async (): Promise<void> => {
           tensor(testFeature), // test input features.
           10 // number of nearest neighbors to consider.
         );
+
         // If there are test labels.
         if (testLabels !== undefined) {
           // Calculate the percentage error between predicted and actual labels.
           const error: number =
             ((testLabels[index][0] as number) - result) /
             (testLabels[index][0] as number);
+
           // Log the percentage error to the console
           console.log(
             `Error for feature ${index}: ${(error * 100).toFixed(2)}%`
@@ -55,7 +70,14 @@ const runKnnAnalysis = async (): Promise<void> => {
   }
 };
 
+/**
+ * Run linear regression analysis.
+ *
+ * Trains a linear regression model using the training data.
+ * Tests the model using test data (if available) and predicts output labels for new data.
+ */
 const runLinearRegressionAnalysis = async (): Promise<void> => {
+  // Load CSV file "cars.csv" containing training and test data
   let {
     features, // input features for training data.
     labels, // output labels for training data.
@@ -73,6 +95,7 @@ const runLinearRegressionAnalysis = async (): Promise<void> => {
     splitTest: 50,
   });
 
+  // Create an instance of LinearRegression class and train the model
   const linearRegression: LinearRegression = new LinearRegression(
     tensor(features),
     tensor(labels),
@@ -81,13 +104,21 @@ const runLinearRegressionAnalysis = async (): Promise<void> => {
 
   linearRegression.train();
 
+  // Test the model using test data (if available)
   if (testFeatures && testLabels) {
     linearRegression.test(tensor(testFeatures), tensor(testLabels));
   }
 
+  // Predict output labels for new data
   linearRegression.predict(tensor([[120, 2, 380]])).print();
 };
 
+/**
+ * Run binary logistic regression analysis.
+ *
+ * Trains a binary logistic regression model using the training data.
+ * Tests the model using test data (if available) and predicts output labels for new data.
+ */
 const runBinaryLogisticRegressionAnalysis = async (): Promise<void> => {
   let {
     features, // input features for training data.
@@ -111,6 +142,7 @@ const runBinaryLogisticRegressionAnalysis = async (): Promise<void> => {
     },
   });
 
+  // Create an instance of BinaryLogisticRegression class and train the model
   const binaryLogisticRegression: BinaryLogisticRegression =
     new BinaryLogisticRegression(tensor(features), tensor(labels), {
       learningRate: 0.5,
@@ -121,15 +153,24 @@ const runBinaryLogisticRegressionAnalysis = async (): Promise<void> => {
 
   binaryLogisticRegression.train();
 
+  // Test the model using test data (if available)
   if (testFeatures && testLabels) {
     binaryLogisticRegression.test(tensor(testFeatures), tensor(testLabels));
   }
 
+  // Predict output labels for new data
   binaryLogisticRegression.predict(tensor([[88, 1.065, 127]])).print();
   binaryLogisticRegression.predict(tensor([[120, 2, 380]])).print();
 };
 
+/**
+ * Run multinominal logistic regression analysis.
+ *
+ * Trains a multinominal logistic regression model using the training data.
+ * Tests the model using test data (if available) and predicts output labels for new data.
+ */
 const runMultinominalLogisticRegressionAnalysis = async (): Promise<void> => {
+  // Load CSV file "cars.csv" containing training and test data
   let {
     features, // input features for training data.
     labels, // output labels for training data.
@@ -159,6 +200,7 @@ const runMultinominalLogisticRegressionAnalysis = async (): Promise<void> => {
     },
   });
 
+  // Create an instance of MultinominalLogisticRegression class and train the model
   const multinominalLogisticRegression: MultinominalLogisticRegression =
     new MultinominalLogisticRegression(
       tensor(features),
@@ -172,6 +214,7 @@ const runMultinominalLogisticRegressionAnalysis = async (): Promise<void> => {
 
   multinominalLogisticRegression.train();
 
+  // Test the model using test data (if available)
   if (testFeatures && testLabels) {
     console.log(
       multinominalLogisticRegression.test(
@@ -181,12 +224,66 @@ const runMultinominalLogisticRegressionAnalysis = async (): Promise<void> => {
     );
   }
 
+  // Predict output labels for new data
   multinominalLogisticRegression.predict(tensor([[150, 2.223, 200]])).print();
 };
 
+/**
+ * Run image recognition analysis.
+ *
+ * Trains a multinominal logistic regression model for image recognition using the MNIST dataset.
+ * Tests the model using test data and outputs the accuracy.
+ */
 const runImageRecognition = (): void => {
-  const mnistData = training(0, 1);
-  console.log(mnistData.images.values);
+  // Load MNIST training data (5000 images)
+  const mnistData: any = mnist.training(0, 5000);
+  // Load MNIST test data (100 images)
+  const testMnistData: any = mnist.testing(0, 100);
+
+  // Extract features from training data
+  const features: Array<Array<number>> = mnistData.images.values.map(
+    (image: any) => flatMap(image)
+  );
+  // Extract labels from training data and convert them to one-hot encoded vectors
+  const labels: Array<Array<number>> = mnistData.labels.values.map(
+    (label: any) => {
+      const row: Array<number> = new Array(10).fill(0);
+      row[label] = 1;
+      return row;
+    }
+  );
+
+  // Extract features from test data
+  const testFeatures: Array<Array<number>> = testMnistData.images.values.map(
+    (image: any) => flatMap(image)
+  );
+  // Extract labels from test data and convert them to one-hot encoded vectors
+  const testLabels: Array<Array<number>> = testMnistData.labels.values.map(
+    (label: any) => {
+      const row: Array<number> = new Array(10).fill(0);
+      row[label] = 1;
+      return row;
+    }
+  );
+
+  // Create an instance of MultinominalLogisticRegression class and train the model
+  const multinominalLogisticRegression: MultinominalLogisticRegression =
+    new MultinominalLogisticRegression(tensor(features), tensor(labels), {
+      learningRate: 1,
+      iterations: 20,
+      batchSize: 100,
+    });
+
+  multinominalLogisticRegression.train();
+
+  // Test the model using test data and output the accuracy
+  console.log(
+    "Accuracy is: ",
+    multinominalLogisticRegression.test(
+      tensor(testFeatures),
+      tensor(testLabels)
+    )
+  );
 };
 
 export {
